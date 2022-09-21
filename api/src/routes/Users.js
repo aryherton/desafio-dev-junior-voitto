513cc2eb6920e6afaf0ec21f2678530535437db3
Ary Barbosa <aryhertonbarbosa@gmail.com>
2022-09-21 14:47:18 -0300

    refactor: imported "ValidUser" in folder "./api/src/routes/Users.js"

--- api/src/routes/Users.js
+++ api/src/routes/Users.js
@@ -1,12 +1,14 @@
 import { Router } from 'express';
 
 import UserController from '../app/controllers/UserController';
+import ValidUser from '../middlewares/ValidUser';
 
 const routes = new Router();
 
 routes
   .get('/', UserController.findAll)
-  .post('/', UserController.create)
+  .post('/', ValidUser.validUserRegister, UserController.create)
+  .post('/login', ValidUser.validUserLogin, UserController.login)
   .delete('/:id', UserController.delete)
   .put('/', UserController.update);
 
