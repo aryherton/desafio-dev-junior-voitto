d63ddf6d670d07aad2f11ef96518e53cecf33139
Ary Barbosa <aryhertonbarbosa@gmail.com>
2022-09-21 19:43:05 -0300

    feat: created router "get(/:id)" in file "./api/src/routes/Cursos.js"

--- api/src/routes/Cursos.js
+++ api/src/routes/Cursos.js
@@ -5,6 +5,7 @@ import CursosController from '../app/controllers/CursoController';
 const routes = new Router();
 
 routes
+  .get('/:id', CursosController.findByIdCurso)
   .get('/', CursosController.findAll)
   .post('/', CursosController.create)
   .delete('/:id', CursosController.delete)
