56c8d78de674761901d7bf7e933b4e30bbd16028
Ary Barbosa <aryhertonbarbosa@gmail.com>
2022-09-20 22:56:47 -0300

    feat: import "Users" in file "./api/src/routes/index.js"

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -2,11 +2,13 @@ import { Router } from 'express';
 
 import Alunos from './Alunos';
 import Cursos from './Cursos';
+import Users from './Users';
 
 const routes = new Router();
 
 routes
   .use('/alunos', Alunos)
-  .use('/cursos', Cursos);
+  .use('/cursos', Cursos)
+  .use('/users', Users);
 
 export default routes;
