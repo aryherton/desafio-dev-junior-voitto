9a75e3d31b5521b60baf0f4c43304fad085498a5
Ary Barbosa <aryhertonbarbosa@gmail.com>
2022-09-23 09:58:32 -0300

    refactor: implemented file "index.tsx" in folder "./web/src/pages/logic-test"

--- web/src/pages/logic-test/index.tsx
+++ web/src/pages/logic-test/index.tsx
@@ -1,9 +1,26 @@
+import React, { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+
 import LogicTestForm from '@/components/Organisms/Forms/LogicTestForm';
 import Dashboard from '@/components/Templates/Layouts/Dashboard';
 import { logicTestText } from '@/constants/Texts';
-import React from 'react';
+import Modal from '@/components/Modal';
+import LoginForm from '@/components/Organisms/Forms/LoginForm';
 
 const Operations: React.FC = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const user = useSelector<IUser>((state: any) => state.user);
+
+  useEffect(() => {
+    const token = JSON.parse(localStorage.getItem('token'));
+
+    if (!token) {
+      setIsModalOpen(true);
+    } else {
+      setIsModalOpen(false);
+    }
+  }, [user]);
+
   return (
     <Dashboard
       title="Teste de Lógica"
@@ -14,6 +31,11 @@ const Operations: React.FC = () => {
         </>
       }
     >
+      {isModalOpen && (
+        <Modal>
+          <LoginForm />
+        </Modal>
+      )}
       <LogicTestForm />
     </Dashboard>
   );
