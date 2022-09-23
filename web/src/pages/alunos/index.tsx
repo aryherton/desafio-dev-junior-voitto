f5090f7179f5f47db6b1bdc5228a9e86a9d6da55
Ary Barbosa <aryhertonbarbosa@gmail.com>
2022-09-23 09:57:47 -0300

    refactor: implemente file "index.tsx" in folder "./web/src/pages/alunos"

--- web/src/pages/alunos/index.tsx
+++ web/src/pages/alunos/index.tsx
@@ -1,8 +1,26 @@
+import React, { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+
 import StudentTable from '@/components/Organisms/Tables/StudentTable';
 import Dashboard from '@/components/Templates/Layouts/Dashboard';
-import React from 'react';
+import Modal from '@/components/Modal';
+import LoginForm from '@/components/Organisms/Forms/LoginForm';
+// import { registerUser } from '@/services/api';
 
 const Operations: React.FC = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const user = useSelector<IUser>((state: any) => state.user);
+
+  useEffect(() => {
+    const token = JSON.parse(localStorage.getItem('token'));
+
+    if (!token) {
+      setIsModalOpen(true);
+    } else {
+      setIsModalOpen(false);
+    }
+  }, [user]);
+
   return (
     <Dashboard
       title="Alunos"
@@ -10,6 +28,11 @@ const Operations: React.FC = () => {
         <p>Aqui você terá acesso à todos os alunos do nosso banco de dados</p>
       }
     >
+      {isModalOpen && (
+        <Modal>
+          <LoginForm />
+        </Modal>
+      )}
       <StudentTable />
     </Dashboard>
   );
