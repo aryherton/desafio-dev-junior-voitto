b8c39d642db91ea8c96c8e49310911b69fc59dbf
Ary Barbosa <aryhertonbarbosa@gmail.com>
2022-09-23 09:59:31 -0300

    refactor: implemented file "index.tsx" in folder "./web/src/pages/seo"

--- web/src/pages/seo/index.tsx
+++ web/src/pages/seo/index.tsx
@@ -1,8 +1,25 @@
+import React, { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+
 import PostsOfBlog from '@/components/Organisms/PostsOfBlog';
 import Dashboard from '@/components/Templates/Layouts/Dashboard';
-import React from 'react';
+import Modal from '@/components/Modal';
+import LoginForm from '@/components/Organisms/Forms/LoginForm';
 
 const SEOPage: React.FC = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const user = useSelector<IUser>((state: any) => state.user);
+
+  useEffect(() => {
+    const token = JSON.parse(localStorage.getItem('token'));
+    
+    if (!token) {
+      setIsModalOpen(true);
+    } else {
+      setIsModalOpen(false);
+    }    
+  }, [user]);
+
   return (
     <Dashboard
       title="SEO Test"
@@ -10,6 +27,11 @@ const SEOPage: React.FC = () => {
         <p>Aqui você terá acesso à um post de artigo. Otimize-o o máximo que conseguir. Lembre-se é que a página seja bem vista pelo google, ou seja, não se esqueça da acessibilidade</p>
       }
     >
+      {isModalOpen && (
+        <Modal>
+          <LoginForm />
+        </Modal>
+      )}
       <PostsOfBlog />
     </Dashboard>
   );
