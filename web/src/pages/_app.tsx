878f3ac727a5952c0f4abc7914cf6730a88652d0
Ary Barbosa <aryhertonbarbosa@gmail.com>
2022-09-23 09:56:07 -0300

    refactor: implemented "Provider" in file "./web/src/pages/_app.tsx"

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import { AppProps } from 'next/app';
 
+import { Provider } from 'react-redux';
+import store from '../redux/store';
 import GlobalStyle from '@/styles/globals';
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
   return (
     <>
-      <Component {...pageProps} />
+      <Provider store={store}>
+        <Component {...pageProps} />
+      </Provider>
       <GlobalStyle />
     </>
   );
