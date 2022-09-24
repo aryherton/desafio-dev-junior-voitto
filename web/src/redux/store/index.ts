0723425a311b002c0599dbd13680e53a3618751d
Ary Barbosa <aryhertonbarbosa@gmail.com>
2022-09-24 12:56:25 -0300

    feat: imported "studentReducer" in file "./web/src/redux/store/index.ts"

--- web/src/redux/store/index.ts
+++ web/src/redux/store/index.ts
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 
 import userReducer from '../slice/userSlice';
+import studentReducer from '../slice/studentSlice';
 
 export default configureStore({
   reducer: {
     user: userReducer,
+    student: studentReducer,
   },
 })
