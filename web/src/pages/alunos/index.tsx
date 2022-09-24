d2b8920923ea7e5fbab035625776024f77f9af0d
Ary Barbosa <aryhertonbarbosa@gmail.com>
2022-09-24 12:49:51 -0300

    refactor: imported and implemented "getAlunos" in file "./web/src/pages/alunos/index.tsx"

--- web/src/pages/alunos/index.tsx
+++ web/src/pages/alunos/index.tsx
@@ -1,39 +1,35 @@
 import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { useRouter } from 'next/dist/client/router';
 
 import StudentTable from '@/components/Organisms/Tables/StudentTable';
 import Dashboard from '@/components/Templates/Layouts/Dashboard';
 import Modal from '@/components/Modal';
 import LoginForm from '@/components/Organisms/Forms/LoginForm';
-import { getUserByToken } from '@/services/api';
+import { getUserByToken, getAlunos } from '@/services/api';
 import { changeUser } from '../../redux/slice/userSlice';
+import { changeArrStudents } from '../../redux/slice/studentSlice';
 
 const Operations: React.FC = () => {
   const dispatch = useDispatch();
-  const router = useRouter();
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const user = useSelector<IUser>((state: any) => state.user.user);
+  const user = useSelector<IUser>((state: any) => state.user.user);  
 
   useEffect(() => {
     const keyToken = localStorage.getItem('token');
 
-    if (!keyToken) {
-      setIsModalOpen(true);
-    } else if (user.admin) {
-      setIsModalOpen(false);
-    } else {
-      router.push('/');
-    }
+    if (!keyToken) setIsModalOpen(true);
 
     if (keyToken && !user) {
       const { token } = keyToken && JSON.parse(keyToken);
       (async () => {
         const user = await getUserByToken('/users/bytoken', token);
+        const arrAlunos = await getAlunos('/alunos', token);
+        setIsModalOpen(false);
+        dispatch(changeArrStudents(arrAlunos));
         dispatch(changeUser(user));
       })();
     }
-  }, [user]);  
+  }, []);  
   
   return (
     <Dashboard
